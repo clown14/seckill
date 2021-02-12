225ef8a58b397ffb8c2ad4e3d5eb823d65cb3aa9
clown <785613198@qq.com>
2021-02-12 15:20:10 +0800

    逆向生成

---
+++ src/main/java/com/xxxx/seckill/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.xxxx.seckill.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xxxx.seckill.pojo.User;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author ssscy
+ * @since 2021-02-12
+ */
+public interface IUserService extends IService<User> {
+
+}
