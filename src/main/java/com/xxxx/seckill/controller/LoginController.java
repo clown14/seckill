79e979c16515725933e467c3cbf61023710a46ce
clown <785613198@qq.com>
2021-02-13 21:26:05 +0800

    开发前期准备

---
+++ src/main/java/com/xxxx/seckill/controller/LoginController.java
@@ -0,0 +1,48 @@
+package com.xxxx.seckill.controller;
+
+import com.xxxx.seckill.service.IUserService;
+import com.xxxx.seckill.vo.RespBean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 登录
+ *
+ * @program: seckill-demo
+ * @author: onion
+ * @create: 2021-02-13 21:07
+ **/
+
+@RestController
+@RequestMapping("/login")
+@Slf4j
+public class LoginController {
+    @Autowired
+    private IUserService userService;
+
+    /**
+     * 功能描述: 跳转登录页面
+     */
+    @RequestMapping("/toLogin")
+    public String toLogin(){
+        return "login";
+    }
+
+    /**
+     * 功能描述: 登录功能
+     */
+    @RequestMapping("/doLogin")
+    @ResponseBody
+    public RespBean doLogin(@Valid LoginVo loginVo, HttpServletRequest request, HttpServletResponse response){
+        return userService.doLogin(loginVo,request,response);
+    }
+
+}
+
+    
---
+++ src/main/java/com/xxxx/seckill/vo/RespBean.java
@@ -0,0 +1,50 @@
+package com.xxxx.seckill.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 公共返回对象
+ * 乐字节：专注线上IT培训
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RespBean {
+
+	private long code;
+	private String message;
+	private Object obj;
+
+	/**
+	 * 功能描述: 成功返回结果
+	 */
+	public static com.xxxx.seckill.vo.RespBean success(){
+		return new com.xxxx.seckill.vo.RespBean(RespBeanEnum.SUCCESS.getCode(),RespBeanEnum.SUCCESS.getMessage(),null);
+	}
+
+	/**
+	 * 功能描述: 成功返回结果
+	 */
+	public static com.xxxx.seckill.vo.RespBean success(Object obj){
+		return new com.xxxx.seckill.vo.RespBean(RespBeanEnum.SUCCESS.getCode(), com.xxxx.seckill.vo.RespBean.success().getMessage(),obj);
+	}
+
+
+	/**
+	 * 功能描述: 失败返回结果
+	 */
+	public static com.xxxx.seckill.vo.RespBean error(RespBeanEnum respBeanEnum){
+		return new com.xxxx.seckill.vo.RespBean(respBeanEnum.getCode(),respBeanEnum.getMessage(),null);
+	}
+
+
+	/**
+	 * 功能描述: 失败返回结果
+	 */
+	public static com.xxxx.seckill.vo.RespBean error(RespBeanEnum respBeanEnum, Object obj){
+		return new com.xxxx.seckill.vo.RespBean(respBeanEnum.getCode(),respBeanEnum.getMessage(),obj);
+	}
+
+}
