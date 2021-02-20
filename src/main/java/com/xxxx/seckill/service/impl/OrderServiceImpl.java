3718c9ff6167a0dce7ee764931f37d2aabefb797
clown <785613198@qq.com>
2021-02-20 16:14:16 +0800

    秒杀功能

--- src/main/java/com/xxxx/seckill/service/impl/OrderServiceImpl.java
+++ src/main/java/com/xxxx/seckill/service/impl/OrderServiceImpl.java
@@ -1,14 +1,24 @@
 package com.xxxx.seckill.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xxxx.seckill.mapper.OrderMapper;
 import com.xxxx.seckill.pojo.Order;
+import com.xxxx.seckill.pojo.SeckillGoods;
+import com.xxxx.seckill.pojo.SeckillOrder;
+import com.xxxx.seckill.pojo.User;
 import com.xxxx.seckill.service.IOrderService;
+import com.xxxx.seckill.service.ISeckillGoodsService;
+import com.xxxx.seckill.service.ISeckillOrderService;
+import com.xxxx.seckill.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author ssscy
@@ -16,5 +26,52 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements IOrderService {
+
+    @Autowired
+    private ISeckillGoodsService seckillGoodsService;
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Autowired
+    private ISeckillOrderService seckillOrderService;
+
+    /**
+     * 秒杀
+     *
+     * @param user
+     * @param goods
+     * @return
+     */
+    @Override
+    public Order seckill(User user, GoodsVo goods) {
+        //秒杀商品表
+        SeckillGoods seckillGoods = seckillGoodsService.getOne(
+                new QueryWrapper<SeckillGoods>().eq("goods_id", goods.getId())
+        );
+        seckillGoods.setStockCount(seckillGoods.getStockCount() - 1);
+        seckillGoodsService.updateById(seckillGoods);
+
+        //生成订单
+        Order order = new Order();
+        order.setUserId(user.getId());
+        order.setGoodsId(goods.getId());
+        order.setDeliveryAddrId(0L);
+        order.setGoodsName(goods.getGoodsName());
+        order.setGoodsCount(1);
+        order.setGoodsPrice(goods.getGoodsPrice());
+        order.setOrderChannel(1);
+        order.setStatus(0);
+        order.setCreateDate(new Date());
+        orderMapper.insert(order);
+
+        //生成秒杀订单
+        SeckillOrder seckillOrder = new SeckillOrder();
+        seckillOrder.setUserId(user.getId());
+        seckillOrder.setGoodsId(goods.getId());
+        seckillOrder.setOrderId(order.getId());
+        seckillOrderService.save(seckillOrder);
+        return order;
+    }
 
 }
