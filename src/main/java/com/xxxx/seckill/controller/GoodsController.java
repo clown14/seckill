ba117999c14e79bab234dbb5389143aa32a52c47
clown <785613198@qq.com>
2021-02-20 15:12:47 +0800

    秒杀倒计时处理

--- src/main/java/com/xxxx/seckill/controller/GoodsController.java
+++ src/main/java/com/xxxx/seckill/controller/GoodsController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+
 /**
  * @program: seckill-demo
  * @author: onion
@@ -57,7 +59,29 @@ public class GoodsController {
     public String toDetail(Model model, User user, @PathVariable Long goodsId) {
 
         model.addAttribute("user", user);
-        model.addAttribute("goods", goodsService.findGoodsVoByGoodsId(goodsId));
+        GoodsVo goodsVo= goodsService.findGoodsVoByGoodsId(goodsId);
+        Date startDate = goodsVo.getStartDate();
+        Date endDate = goodsVo.getEndDate();
+        Date nowDate = new Date();
+        //秒杀状态
+        int secKillStatus = 0;
+        //秒杀倒计时
+        int remainSeconds = 0;
+        //秒杀还未开始
+        if (nowDate.before(startDate)) {
+            remainSeconds = ((int) ((startDate.getTime() - nowDate.getTime()) / 1000));
+        } else if (nowDate.after(endDate)) {
+            //	秒杀已结束
+            secKillStatus = 2;
+            remainSeconds = -1;
+        } else {
+            //秒杀中
+            secKillStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("goods", goodsVo);
+        model.addAttribute("secKillStatus", secKillStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
         return "goodsDetail";
     }
 }
